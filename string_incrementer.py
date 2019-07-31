269d8c67e54516d6825e0963f9b7e2fdea7db94a
Obi Wan <hellothere@gmail.com>
2019-07-31 14:08:38 +0300

    Final answer

--- string_incrementer.py
+++ string_incrementer.py
@@ -1,16 +1,15 @@
 #https://www.codewars.com/kata/54a91a4883a7de5d7800009c/train/python
 def increment_string(strng):
     import re
-    patt = r"([0-9]+)"
-    out = re.search(patt,strng)
-        
-    if out:
-        out = out.group()
-        index = strng.find(out)
-        return f"{strng[:index]}{out[:-1]}{int(out[-1]) + 1 }"     
-    else:
-        return strng + "1"
-
+    nb = re.findall(r'\d+',strng)
+    if nb:
+        new_nb = nb[-1]
+        strng = strng.rsplit(new_nb,1)[0]
+        nb = str(int(new_nb)+1)
+        return strng + '0' * (len(new_nb) - len(nb)) + nb
+    return strng + '1'
 
 print(increment_string("foobar001"))
 #print(increment_string("bar"))
+print(increment_string("foobar99"))
+print(increment_string("foobar099"))
